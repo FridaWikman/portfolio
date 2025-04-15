8c466b49e0898870e5daacbc826dda5c44f8c208
FridaWikman <frida.wikman@hotmail.com>
2025-04-15 15:43:21 +0200

    About section and changed padding. Link to About me.

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,20 @@ import { Icon } from '@iconify/react'
 function App() {
   return (
     <>
-      <header className="fixed top-0 right-0 p-6 z-50">
+      <header className="fixed top-0 right-0 py-6 z-50">
         <nav className="flex items-center gap-8">
-          <a href="#about" className="text-base font-medium hover:underline">
+          <a href="#about" className="text-base font-medium accent-hover">
             About
           </a>
-          <a href="#projects" className="text-base font-medium hover:underline">
+          <a href="#projects" className="text-base font-medium accent-hover">
             Projects
           </a>
-          <div className="relative">
-            <a
-              href="#contact"
-              className="text-base font-medium hover:underline"
-            >
-              Contact
-            </a>
-            {/* <div className="accent-color w-px h-24 border-l border border-accent mt-4"></div> */}
-          </div>
+          {/* <div className="relative"> */}
+          <a href="#contact" className="text-base font-medium accent-hover">
+            Contact
+          </a>
+          <div className="accent-color w-24 border-l border border-accent"></div>
+          {/* </div> */}
         </nav>
       </header>
       <div className="fixed bottom-0 left-6 flex flex-col items-center gap-4 z-50">
@@ -35,18 +32,20 @@ function App() {
         <a href="" className="accent-hover hover:rotate-6 transition-transform">
           <Icon icon="hugeicons:linkedin-02" width="25" height="25" />
         </a>
-
-        {/* Prickat streck */}
         <div className="accent-color w-px h-24 border-l border border-accent mt-4"></div>
       </div>
-      <main className="min-h-screen pt-10 mx-7">
-        <div className="lg:p-30">
-          <div className="mb-16 lg:pr-16">
-            <h1 className="accent-color text-base">Hello, my name is</h1>
-            <h2 className="text-8xl font-semibold leading-none">
+
+      <main className="min-h-screen pt-10 mx-40">
+        <section className="p-20 min-h-screen">
+          <div className="mb-16 lg:pr-14 pt-10">
+            <h1 className="mb-4">
+              <span className="accent-color">Hello,</span>
+              <span className="opacity-60"> my name is</span>
+            </h1>
+            <h2 className="accent-color text-7xl font-semibold text-nowrap">
               Frida Wikman.
             </h2>
-            <h3 className="text-4xl font-semibold opacity-60 leading-none">
+            <h3 className="text-4xl font-semibold opacity-60">
               I'm a web developer.
             </h3>
             {/* <div className="mt-16 flex justify-between max-w-[60px]">
@@ -75,7 +74,18 @@ function App() {
             omnis ex dolorum tenetur nobis commodi nisi.
           </p>
         </div> */}
-        </div>
+        </section>
+        <section id="about" className="p-20 max-w-9/10 ">
+          <h4 id="about" className="text-4xl font-bold">
+            About me
+          </h4>
+          <p className="text-base  opacity-60 leading-normal">
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum
+            repellendus numquam ratione dignissimos, inventore mollitia
+            voluptatibus sapiente nostrum aliquid soluta beatae minus ipsum
+            omnis ex dolorum tenetur nobis commodi nisi.
+          </p>
+        </section>
       </main>
     </>
   )
